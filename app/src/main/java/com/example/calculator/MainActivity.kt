e9f58ef4b20d37b58165fc6b04d88140bd7865e1
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-04-26 16:23:38 -0300

    refactoring synthetic extension ids by viewbinding

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,64 +2,69 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calculator.databinding.ActivityMainBinding
 import io.kaen.dagger.ExpressionParser
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         supportActionBar!!.hide()
 
         // numbers
-        buttonZero.setOnClickListener { receiveValue("0") }
-        buttonOne.setOnClickListener { receiveValue("1") }
-        buttonTwo.setOnClickListener { receiveValue("2") }
-        buttonThree.setOnClickListener { receiveValue("3") }
-        buttonFour.setOnClickListener { receiveValue("4") }
-        buttonFive.setOnClickListener { receiveValue("5") }
-        buttonSix.setOnClickListener { receiveValue("6") }
-        buttonSeven.setOnClickListener { receiveValue("7") }
-        buttonEight.setOnClickListener { receiveValue("8") }
-        buttonNine.setOnClickListener { receiveValue("9") }
+        binding.buttonZero.setOnClickListener { receiveValue("0") }
+        binding.buttonOne.setOnClickListener { receiveValue("1") }
+        binding.buttonTwo.setOnClickListener { receiveValue("2") }
+        binding.buttonThree.setOnClickListener { receiveValue("3") }
+        binding.buttonFour.setOnClickListener { receiveValue("4") }
+        binding.buttonFive.setOnClickListener { receiveValue("5") }
+        binding.buttonSix.setOnClickListener { receiveValue("6") }
+        binding.buttonSeven.setOnClickListener { receiveValue("7") }
+        binding.buttonEight.setOnClickListener { receiveValue("8") }
+        binding.buttonNine.setOnClickListener { receiveValue("9") }
 
         // operators
-        buttonAdd.setOnClickListener { receiveValue("+") }
-        buttonSub.setOnClickListener { receiveValue("-") }
-        buttonMult.setOnClickListener { receiveValue("*") }
-        buttonDiv.setOnClickListener { receiveValue("/") }
+        binding.buttonAdd.setOnClickListener { receiveValue("+") }
+        binding.buttonSub.setOnClickListener { receiveValue("-") }
+        binding.buttonMult.setOnClickListener { receiveValue("*") }
+        binding.buttonDiv.setOnClickListener { receiveValue("/") }
 
         // dot
-        buttonDot.setOnClickListener { receiveValue(".") }
+        binding.buttonDot.setOnClickListener { receiveValue(".") }
 
         // del
-        buttonDel.setOnClickListener {
-            textExpression.text = ""
-            textResult.text = ""
+        binding.buttonDel.setOnClickListener {
+            binding.textExpression.text = ""
+            binding.textResult.text = ""
         }
 
         // open and close
-        buttonOpen.setOnClickListener { receiveValue("(") }
-        buttonClose.setOnClickListener { receiveValue(")") }
+        binding.buttonOpen.setOnClickListener { receiveValue("(") }
+        binding.buttonClose.setOnClickListener { receiveValue(")") }
 
         // equal
-        buttonEqual.setOnClickListener {
-            textResult.text = getMathResult(textExpression.text.toString())
+        binding.buttonEqual.setOnClickListener {
+            binding.textResult.text = getMathResult(binding.textExpression.text.toString())
         }
     }
 
-    fun receiveValue(value: String): String {
-        if (textResult.text == "") {
-            textExpression.append(value)
+    private fun receiveValue(value: String): String {
+        if (binding.textResult.text == "") {
+            binding.textExpression.append(value)
         }
         return value
     }
 
-    fun getMathResult(expression: String): String {
+    private fun getMathResult(expression: String): String {
         val parser = ExpressionParser()
         val result = parser.evaluate(expression)
         return result.toString()
     }
-
 }
