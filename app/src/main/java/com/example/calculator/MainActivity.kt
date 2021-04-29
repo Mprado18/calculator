887ddcbdc47488da4361dff080eae8a2b36e41dd
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-04-29 14:39:16 -0300

    studying change mvvm

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,69 +2,13 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.calculator.databinding.ActivityMainBinding
-import io.kaen.dagger.ExpressionParser
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+//        setContentView(R.layout.activity_main)
 
         supportActionBar!!.hide()
-
-        // numbers
-        binding.buttonZero.setOnClickListener { receiveValue("0") }
-        binding.buttonOne.setOnClickListener { receiveValue("1") }
-        binding.buttonTwo.setOnClickListener { receiveValue("2") }
-        binding.buttonThree.setOnClickListener { receiveValue("3") }
-        binding.buttonFour.setOnClickListener { receiveValue("4") }
-        binding.buttonFive.setOnClickListener { receiveValue("5") }
-        binding.buttonSix.setOnClickListener { receiveValue("6") }
-        binding.buttonSeven.setOnClickListener { receiveValue("7") }
-        binding.buttonEight.setOnClickListener { receiveValue("8") }
-        binding.buttonNine.setOnClickListener { receiveValue("9") }
-
-        // operators
-        binding.buttonAdd.setOnClickListener { receiveValue("+") }
-        binding.buttonSub.setOnClickListener { receiveValue("-") }
-        binding.buttonMult.setOnClickListener { receiveValue("*") }
-        binding.buttonDiv.setOnClickListener { receiveValue("/") }
-
-        // dot
-        binding.buttonDot.setOnClickListener { receiveValue(".") }
-
-        // del
-        binding.buttonDel.setOnClickListener {
-            binding.textExpression.text = ""
-            binding.textResult.text = ""
-        }
-
-        // open and close
-        binding.buttonOpen.setOnClickListener { receiveValue("(") }
-        binding.buttonClose.setOnClickListener { receiveValue(")") }
-
-        // equal
-        binding.buttonEqual.setOnClickListener {
-            binding.textResult.text = getMathResult(binding.textExpression.text.toString())
-        }
-    }
-
-    private fun receiveValue(value: String): String {
-        if (binding.textResult.text == "") {
-            binding.textExpression.append(value)
-        }
-        return value
-    }
-
-    private fun getMathResult(expression: String): String {
-        val parser = ExpressionParser()
-        val result = parser.evaluate(expression)
-        return result.toString()
     }
 }
---
+++ app/src/main/java/com/example/calculator/viewModel/MainViewModel.kt
@@ -0,0 +1,3 @@
+package com.example.calculator.viewModel
+
+// não consegui pensar em uma forma de chamar a lógica aqui para ativar no MainActivity
